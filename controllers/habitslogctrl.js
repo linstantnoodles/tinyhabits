a421e81196f92cf80ee83068b473a4b16e39b9d4
Alan <alin@2u.com>
2014-08-24 18:18:52 -0400

    having to set model properties in two different controllers seems confusing

--- controllers/habitslogctrl.js
+++ controllers/habitslogctrl.js
@@ -10,14 +10,6 @@ app.controller('HabitsLogController', [
 
     $scope.habitHistory = null;
 
-    for (var i = 0; i < $scope.habits.length; i++) {
-      if (!$scope.habits[i].stats[currUTCdate]) {
-        $scope.habits[i].stats[currUTCdate] = {
-          status: ""
-        };
-      }
-    }
-
     $scope.$watch("habits", function(newVal, oldVal) {
       localStorageService.set("habits", $scope.habits);
     }, true);
